338367e22a754d619fe83c68f07fc49391e6cecb
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-12-30 18:45:48 +0200

    Get started

--- src/main/java/me/gregzee/dupe/commands/DupeCommand.java
+++ src/main/java/me/gregzee/dupe/commands/DupeCommand.java
@@ -1,13 +1,37 @@
 package me.gregzee.dupe.commands;
 
+import me.gregzee.dupe.config.ConfigManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public final class DupeCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+		if (!(sender instanceof Player player)) {
+			sender.sendMessage(ConfigManager.Messages.getOnlyPlayers());
+			return true;
+		}
+
+		if (!player.hasPermission(ConfigManager.Permissions.getDupe())) {
+			sender.sendMessage(ConfigManager.Messages.getNoPermission());
+			return true;
+		}
+
+		if (args.length == 1) {
+
+		} else if (args.length == 0) {
+			
+		} else {
+			player.sendMessage(Component.text("").color(NamedTextColor.RED));
+			return true;
+		}
+
+
 		return true;
 	}
 }
