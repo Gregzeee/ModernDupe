f46d373967d70fa755796821d99f2b0f989906eb
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-12-30 13:41:32 +0200

    Create DupeCommand class

---
+++ src/main/java/me/gregzee/dupe/commands/DupeCommand.java
@@ -0,0 +1,13 @@
+package me.gregzee.dupe.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class DupeCommand implements CommandExecutor {
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+		return true;
+	}
+}
