90d6a0ca6ff790cb2ce47c0b935d541c25e6dfb4
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-12-30 13:52:11 +0200

    Make the class final

--- src/main/java/me/gregzee/dupe/commands/DupeCommand.java
+++ src/main/java/me/gregzee/dupe/commands/DupeCommand.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-public class DupeCommand implements CommandExecutor {
+public final class DupeCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 		return true;
