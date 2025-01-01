018465595e48875c9adff8403c68190605bf59e4
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2025-01-01 20:32:50 +0200

    Add method for checking if an item is an ExcellentCrates key

--- src/main/java/me/gregzee/moderndupe/util/DupeUtil.java
+++ src/main/java/me/gregzee/moderndupe/util/DupeUtil.java
@@ -1,4 +1,20 @@
 package me.gregzee.moderndupe.util;
 
+import org.bukkit.inventory.ItemStack;
+import su.nightexpress.excellentcrates.CratesAPI;
+
 public final class DupeUtil {
+
+	/**
+	 * Determines if the given item is a crate key
+	 * @param item the item to check
+	 * @return {@code true} if the item is a key; {@code false} otherwise
+	 */
+	public static boolean isCrateKey(ItemStack item) {
+		if (item == null) {
+			return false;
+		}
+
+		return CratesAPI.getKeyManager().isKey(item);
+	}
 }
