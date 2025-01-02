09dd3ad690918c2a65c5e7ebd1ab44c638bb4f01
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2025-01-02 16:41:23 +0200

    Add multiple new methods

--- src/main/java/me/gregzee/moderndupe/util/DupeUtil.java
+++ src/main/java/me/gregzee/moderndupe/util/DupeUtil.java
@@ -1,7 +1,14 @@
 package me.gregzee.moderndupe.util;
 
+import me.gregzee.moderndupe.config.ConfigManager;
+import org.bukkit.Material;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.strassburger.lifestealz.LifeStealZ;
+import org.strassburger.lifestealz.api.LifeStealZAPI;
 import su.nightexpress.excellentcrates.CratesAPI;
+import java.util.Set;
 
 public final class DupeUtil {
 
@@ -10,11 +17,93 @@ public final class DupeUtil {
 	 * @param item the item to check
 	 * @return {@code true} if the item is a key; {@code false} otherwise
 	 */
-	public static boolean isCrateKey(ItemStack item) {
+	private static boolean isCrateKey(ItemStack item) {
 		if (item == null) {
 			return false;
 		}
 
 		return CratesAPI.getKeyManager().isKey(item);
 	}
+
+	/**
+	 * Determines if the given item is a custom item from LifestealZ
+	 * @param item the item to check
+	 * @return {@code true} if the item is a custom item from LifestealZ; {@code false} otherwise
+	 */
+	private static boolean isLifestealZItem(ItemStack item) {
+		if (item == null) {
+			return false;
+		}
+
+		LifeStealZAPI lifeStealZ = LifeStealZ.getAPI();
+
+		// Get the items custom ID
+		String customID = lifeStealZ.getCustomItemID(item);
+
+		// Get the list of custom items that are from LifeStealZ
+		Set<String> customItemIDs = lifeStealZ.getCustomItemIDs();
+
+		return customItemIDs.contains(customID);
+	}
+
+	/**
+	 * Determines if the given item is blacklisted
+	 * @param item the item to check
+	 * @return {@code true} if the item is blacklisted; {@code false} otherwise
+	 */
+	private static boolean isBlacklisted(ItemStack item) {
+		// Check if the item is null
+		if (item == null) {
+			return false;
+		}
+
+		// Check if the item is in the blacklist
+		if (ConfigManager.getBlacklist().contains(item.getType())) {
+			return true;
+		// Check if the item is a crate key
+		} else if (isCrateKey(item)) {
+			return true;
+		// Check if the item is a LifeStealZ Item
+		} else {
+			return isLifestealZItem(item);
+		}
+	}
+
+	/**
+	 * Loops through the contents of the given shulker box and checks for any items that are blacklisted
+	 * @param shulkerBox the shulker box of which to check the contents of
+	 * @return {@code true} if a blacklisted item was found; {@code false} otherwise
+	 */
+	private static boolean containsBlacklistedItemsInShulker(ShulkerBox shulkerBox) {
+
+		// Get the entire contents of the shulker
+		ItemStack[] contents = shulkerBox.getInventory().getContents();
+
+		// Loop through the shulkerbox contents
+		for (ItemStack item : contents) {
+
+			if (item == null) continue;
+
+			if (isBlacklisted(item)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public void dupe(Player player) {
+		if (!player.getInventory().getItemInMainHand().getType().equals(Material.SHULKER_BOX)) {
+//			if (!containsBlacklistedItemsInShulker(() player.getInventory().getItemInMainHand())) {
+//
+//			}
+
+
+		}
+	}
+
+	public void dupe(Player player, int count) {
+
+	}
+
 }
